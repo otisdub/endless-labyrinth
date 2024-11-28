3ba137023f6c0358b1622390ab44f5b314c6ca1e
OtisdubYT <otistatewhitaker@icloud.com>
2024-11-28 06:46:16 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function startGame() {
+    // Replace the URL below with the website you want to redirect to
+    window.location.href = "https://www.meta.com/experiences/the-endless-labyrinth/28086685597611557/";
+}
